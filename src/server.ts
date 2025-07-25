7e1f5a4fd50c22fe90688c89616326fb94542c56
ale-montes <amontes@frro.utn.edu.ar>
2025-07-25 17:28:09 -0300

    fix: conflict routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
 import { usuariosRoutes } from './usuarios/usuarios.routes.js';
+import { deidadesRoutes } from './deidades/deidades.routes.js';
+import { cabanasRoutes } from './cabanas/cabanas.routes.js';
+import { talleresRoutes } from './talleres/talleres.routes.js';
 import { jsonErrorHandler } from './shared/jsonErrorHandler.js';
 
 const app = express();
@@ -9,6 +12,9 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/api/usuarios', usuariosRoutes);
+app.use('/api/deidades', deidadesRoutes);
+app.use('/api/cabanas', cabanasRoutes);
+app.use('/api/talleres', talleresRoutes);
 
 app.use(jsonErrorHandler);
 
