963e52f2c9a2889c32bee0121cf7a1b4732dbcae
ale-montes <amontes@frro.utn.edu.ar>
2025-08-22 10:19:19 -0300

    fix entity usuario

--- src/shared/db/baseEntity.entity.ts
+++ src/shared/db/baseEntity.entity.ts
@@ -1,19 +1,17 @@
 import { PrimaryKey } from '@mikro-orm/core';
+import { Property } from '@mikro-orm/core';
+import { DateTimeType } from '@mikro-orm/mysql'; // Adjust import based on your setup
 
 export abstract class BaseEntity {
   @PrimaryKey()
   id?: number;
 
-  /*
-
   @Property({ type: DateTimeType })
-  createdAt? = new Date()
+  createdAt? = new Date();
 
   @Property({
     type: DateTimeType,
     onUpdate: () => new Date(),
   })
-  updatedAt? = new Date()
-
-  */
+  updatedAt? = new Date();
 }
