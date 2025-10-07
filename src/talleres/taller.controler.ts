10032c38bb68b197e6839cc8350063323a551b35
Camila Stella <146795383+mimistella@users.noreply.github.com>
2025-10-07 11:37:25 -0300

    fx: dupliqué sin querer los atributos aparentemente

--- src/talleres/taller.controler.ts
+++ src/talleres/taller.controler.ts
@@ -20,8 +20,6 @@ async function findAll(req: Request, res: Response) {
         nombre: taller.instructor.nombre,
         apellido: taller.instructor.apellido,
       },
-      cupo: taller.cupo,
-      duracionHoras: taller.duracionHoras,
     }));
     res.status(200).json({ message: 'found all talleres', data: talleresSanitizados });
   } catch (error: unknown) {
