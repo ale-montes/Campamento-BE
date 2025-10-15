940abf6519931803f1eecd73bcb70380f81f74c5
ale-montes <amontes@frro.utn.edu.ar>
2025-10-15 11:15:25 -0300

    deidad: refact:schemas y agrego routes middleware valid por role

--- src/deidades/deidad.controler.ts
+++ src/deidades/deidad.controler.ts
@@ -1,89 +1,56 @@
-import { Request, Response } from 'express';
-import { Deidad } from './deidad.entity.js';
-import { orm } from '../shared/db/orm.js';
+import { Request, Response, NextFunction } from 'express';
+import { DeidadService } from './deidad.service.js';
+import { getEm } from '../shared/db/orm.js';
+import { validateId } from '../shared/validateParam.js';
 
-const em = orm.em;
+export class DeidadController {
+  constructor(private readonly service: DeidadService = new DeidadService()) {}
 
-async function findAll(req: Request, res: Response) {
-  try {
-    const deidades = await em.find(Deidad, {});
-    res.status(200).json({ message: 'found all deidades', data: deidades });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-      res.status(500).json({ message: 'Internal server error' });
-    } else {
-      console.log('Unknown error', error);
-      res.status(500).json({ message: 'Unknown error' });
+  async findAll(req: Request, res: Response, next: NextFunction) {
+    try {
+      const deidades = await this.service.findAll(getEm());
+      res.status(200).json({ message: 'found all deidades', data: deidades });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async findOne(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = validateId(req.params.id);
+      const deidad = await this.service.findOne(id, getEm());
+      res.status(200).json({ message: 'found deidad', data: deidad });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async add(req: Request, res: Response, next: NextFunction) {
+    try {
+      await this.service.add(req.body.sanitizedInput, getEm());
+      res.status(201).json({ message: 'deidad created' });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = validateId(req.params.id);
+      await this.service.update(id, req.body.sanitizedInput, getEm());
+      res.status(200).json({ message: 'deidad updated' });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async remove(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = validateId(req.params.id);
+      await this.service.remove(id, getEm());
+      res.status(200).json({ message: 'deidad deleted' });
+    } catch (error) {
+      next(error);
     }
   }
 }
-
-async function findOne(req: Request, res: Response) {
-  try {
-    const id = Number.parseInt(req.params.id);
-    const deidad = await em.findOneOrFail(Deidad, { id });
-    res.status(200).json({ message: 'found deidad', data: deidad });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-      res.status(500).json({ message: 'Internal server error' });
-    } else {
-      console.log('Unknown error', error);
-      res.status(500).json({ message: 'Unknown error' });
-    }
-  }
-}
-
-async function add(req: Request, res: Response) {
-  try {
-    const deidad = em.create(Deidad, req.body);
-    await em.flush();
-    res.status(201).json({ message: 'deidad created', data: deidad });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-      res.status(500).json({ message: 'Internal server error' });
-    } else {
-      console.log('Unknown error', error);
-      res.status(500).json({ message: 'Unknown error' });
-    }
-  }
-}
-
-async function update(req: Request, res: Response) {
-  try {
-    const id = Number.parseInt(req.params.id);
-    const deidad = em.getReference(Deidad, id);
-    em.assign(deidad, req.body);
-    await em.flush();
-    res.status(200).json({ message: 'deidad updated' });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-      res.status(500).json({ message: 'Internal server error' });
-    } else {
-      console.log('Unknown error', error);
-      res.status(500).json({ message: 'Unknown error' });
-    }
-  }
-}
-
-async function remove(req: Request, res: Response) {
-  try {
-    const id = Number.parseInt(req.params.id);
-    const deidad = em.getReference(Deidad, id);
-    await em.removeAndFlush(deidad);
-    res.status(200).send({ message: 'deidad deleted' });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-      res.status(500).json({ message: 'Internal server error' });
-    } else {
-      console.log('Unknown error', error);
-      res.status(500).json({ message: 'Unknown error' });
-    }
-  }
-}
-
-export { findAll, findOne, add, update, remove };
