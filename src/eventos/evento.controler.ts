70e3ea927cde078802762023ead44848ab2bc707
ale-montes <amontes@frro.utn.edu.ar>
2025-10-16 19:23:38 -0300

    refactor eventos y solicitud eventos

--- src/eventos/evento.controler.ts
+++ src/eventos/evento.controler.ts
@@ -1,89 +1,56 @@
-import { Request, Response } from 'express';
-import { Evento } from './evento.entity.js';
-import { orm } from '../shared/db/orm.js';
+import { Request, Response, NextFunction } from 'express';
+import { EventoService } from './evento.service.js';
+import { getEm } from '../shared/db/orm.js';
+import { validateId } from '../shared/validateParam.js';
 
-const em = orm.em;
+export class EventoController {
+  constructor(private readonly service: EventoService = new EventoService()) {}
 
-async function findAll(req: Request, res: Response) {
-  try {
-    const eventos = await em.find(Evento, {});
-    res.status(200).json({ message: 'found all eventos', data: eventos });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-      res.status(500).json({ message: 'Internal server error' });
-    } else {
-      console.log('Unknown error', error);
-      res.status(500).json({ message: 'Unknown error' });
+  async findAll(req: Request, res: Response, next: NextFunction) {
+    try {
+      const eventos = await this.service.findAll(getEm());
+      res.status(200).json({ message: 'found eventos', data: eventos });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async findOne(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = validateId(req.params.id);
+      const evento = await this.service.findOne(id, getEm());
+      res.status(200).json({ message: 'found evento', data: evento });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async add(req: Request, res: Response, next: NextFunction) {
+    try {
+      const evento = await this.service.add(req.body.sanitizedInput, getEm());
+      res.status(201).json({ message: 'evento created', data: evento });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = validateId(req.params.id);
+      await this.service.update(id, req.body.sanitizedInput, getEm());
+      res.status(200).json({ message: 'evento updated' });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async remove(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = validateId(req.params.id);
+      await this.service.remove(id, getEm());
+      res.status(200).json({ message: 'evento deleted' });
+    } catch (error) {
+      next(error);
     }
   }
 }
-
-async function findOne(req: Request, res: Response) {
-  try {
-    const id = Number.parseInt(req.params.id);
-    const evento = await em.findOneOrFail(Evento, { id });
-    res.status(200).json({ message: 'found evento', data: evento });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-      res.status(500).json({ message: 'Internal server error' });
-    } else {
-      console.log('Unknown error', error);
-      res.status(500).json({ message: 'Unknown error' });
-    }
-  }
-}
-
-async function add(req: Request, res: Response) {
-  try {
-    const evento = em.create(Evento, req.body);
-    await em.flush();
-    res.status(201).json({ message: 'evento created', data: evento });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-      res.status(500).json({ message: 'Internal server error' });
-    } else {
-      console.log('Unknown error', error);
-      res.status(500).json({ message: 'Unknown error' });
-    }
-  }
-}
-
-async function update(req: Request, res: Response) {
-  try {
-    const id = Number.parseInt(req.params.id);
-    const evento = em.getReference(Evento, id);
-    em.assign(evento, req.body);
-    await em.flush();
-    res.status(200).json({ message: 'evento updated' });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-      res.status(500).json({ message: 'Internal server error' });
-    } else {
-      console.log('Unknown error', error);
-      res.status(500).json({ message: 'Unknown error' });
-    }
-  }
-}
-
-async function remove(req: Request, res: Response) {
-  try {
-    const id = Number.parseInt(req.params.id);
-    const evento = em.getReference(Evento, id);
-    await em.removeAndFlush(evento);
-    res.status(200).send({ message: 'evento deleted' });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-      res.status(500).json({ message: 'Internal server error' });
-    } else {
-      console.log('Unknown error', error);
-      res.status(500).json({ message: 'Unknown error' });
-    }
-  }
-}
-
-export { findAll, findOne, add, update, remove };
--- src/eventos/solicitud-evento.controler.ts
+++ src/eventos/solicitud-evento.controler.ts
@@ -1,89 +1,63 @@
-import { Request, Response } from 'express';
-import { SolicitudEvento } from './solicitud-evento.entity.js';
-import { orm } from '../shared/db/orm.js';
+import { Request, Response, NextFunction } from 'express';
+import { SolicitudEventoService } from './solicitud-evento.service.js';
+import { getEm } from '../shared/db/orm.js';
+import { validateId } from '../shared/validateParam.js';
+import { UserPayload } from '../types/user.js';
 
-const em = orm.em;
+export class SolicitudEventoController {
+  constructor(private readonly service: SolicitudEventoService = new SolicitudEventoService()) {}
 
-async function findAll(req: Request, res: Response) {
-  try {
-    const solicitudes = await em.find(SolicitudEvento, {}, { populate: ['evento'] });
-    res.status(200).json({ message: 'found all talleres', data: solicitudes });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-      res.status(500).json({ message: 'Internal server error' });
-    } else {
-      console.log('Unknown error', error);
-      res.status(500).json({ message: 'Unknown error' });
+  async findAll(req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = req.user as UserPayload;
+      const solicitudes = await this.service.findAll(user, getEm());
+      res.status(200).json({ message: 'found solicitudes', data: solicitudes });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async findOne(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = validateId(req.params.id);
+      const user = req.user as UserPayload;
+      const solicitud = await this.service.findOne(id, user, getEm());
+      res.status(200).json({ message: 'found solicitud', data: solicitud });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async add(req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = req.user as UserPayload;
+      const input = req.body!.sanitizedInput;
+      await this.service.add(input, user, getEm());
+      res.status(201).json({ message: 'solicitud created' });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = validateId(req.params.id);
+      const user = req.user as UserPayload;
+      const input = req.body!.sanitizedInput;
+      await this.service.update(id, input, user, getEm());
+      res.status(200).json({ message: 'solicitud updated' });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async remove(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = validateId(req.params.id);
+      await this.service.remove(id, getEm());
+      res.status(200).json({ message: 'solicitud deleted' });
+    } catch (error) {
+      next(error);
     }
   }
 }
-
-async function findOne(req: Request, res: Response) {
-  try {
-    const id = Number.parseInt(req.params.id);
-    const solicitud = await em.findOneOrFail(SolicitudEvento, { id }, { populate: ['evento'] });
-    res.status(200).json({ message: 'found solicitud', data: solicitud });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-      res.status(500).json({ message: 'Internal server error' });
-    } else {
-      console.log('Unknown error', error);
-      res.status(500).json({ message: 'Unknown error' });
-    }
-  }
-}
-
-async function add(req: Request, res: Response) {
-  try {
-    const solicitud = em.create(SolicitudEvento, req.body);
-    await em.flush();
-    res.status(201).json({ message: 'solicitud created', data: solicitud });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-      res.status(500).json({ message: 'Internal server error' });
-    } else {
-      console.log('Unknown error', error);
-      res.status(500).json({ message: 'Unknown error' });
-    }
-  }
-}
-
-async function update(req: Request, res: Response) {
-  try {
-    const id = Number.parseInt(req.params.id);
-    const solicitud = await em.findOneOrFail(SolicitudEvento, { id });
-    em.assign(solicitud, req.body);
-    await em.flush();
-    res.status(200).json({ message: 'solicitud updated' });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-      res.status(500).json({ message: 'Internal server error' });
-    } else {
-      console.log('Unknown error', error);
-      res.status(500).json({ message: 'Unknown error' });
-    }
-  }
-}
-
-async function remove(req: Request, res: Response) {
-  try {
-    const id = Number.parseInt(req.params.id);
-    const solicitud = em.getReference(SolicitudEvento, id);
-    await em.removeAndFlush(solicitud);
-    res.status(200).send({ message: 'solicitud deleted' });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-      res.status(500).json({ message: 'Internal server error' });
-    } else {
-      console.log('Unknown error', error);
-      res.status(500).json({ message: 'Unknown error' });
-    }
-  }
-}
-
-export { findAll, findOne, add, update, remove };
